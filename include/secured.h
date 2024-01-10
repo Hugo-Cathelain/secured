026e3af83378d0b02a4a48ac0123f92ebbc03ce6
Hugo <hugo.cathelain@epitech.eu>
2024-01-10 10:44:30 +0100

    [ADD] first commit

---
+++ include/secured.h
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2024
+** B-CPE-110-LYN-1-1-secured-hugo.cathelain
+** File description:
+** secured
+*/
+
+#ifndef SECURED_H_
+    #define SECURED_H_
+
+
+#endif /* !SECURED_H_ */
---
+++ src/main.c
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2024
+** B-CPE-110-LYN-1-1-secured-hugo.cathelain
+** File description:
+** main
+*/
+#include "../include/secured.h"
+
+int main(int ac, char **av)
+{
+    return 0;
+}
